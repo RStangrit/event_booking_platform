a8e49df3cb4c410e54db7a90f7dc8448c12e7318
flameprincess <unpredictableanonymous639@gmail.com>
2025-01-23 14:43:21 +0500

    implement custom response handler

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"main/pkg/util"
 	"net/http"
 	"strconv"
 
@@ -14,23 +15,23 @@ func createUserHandler(context *gin.Context) {
 
 	err := context.ShouldBindJSON(&user)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
+		util.ProvideResponse(context, 400, "could not parse request data")
 	}
 
 	err = user.Save()
 	if err != nil {
 		fmt.Println(err)
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not save user"})
+		util.ProvideResponse(context, 500, "could not save user")
 		return
 	}
 
-	context.JSON(http.StatusCreated, gin.H{"message": "user created successfully"})
+	util.ProvideResponse(context, 201, "user created successfully")
 }
 
 func getUsersHandler(context *gin.Context) {
 	users, err := getAll()
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch users"})
+		util.ProvideResponse(context, 500, "could not fetch users")
 		return
 	}
 	context.JSON(http.StatusOK, users)
@@ -39,13 +40,13 @@ func getUsersHandler(context *gin.Context) {
 func getUserHandler(context *gin.Context) {
 	userId, err := strconv.ParseInt(context.Param("id"), 10, 64)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse user id"})
+		util.ProvideResponse(context, 400, "could not parse user id")
 		return
 	}
 
 	user, err := getOne(userId)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch user"})
+		util.ProvideResponse(context, 400, "could not fetch user")
 		return
 	}
 	context.JSON(http.StatusOK, user)
@@ -54,55 +55,55 @@ func getUserHandler(context *gin.Context) {
 func updateUserHandler(context *gin.Context) {
 	userId, err := strconv.ParseInt(context.Param("id"), 10, 64)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse user id"})
+		util.ProvideResponse(context, 400, "could not parse user id")
 		return
 	}
 
 	user, err := getOne(userId)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch user"})
+		util.ProvideResponse(context, 500, "could not fetch user")
 		return
 	}
 
 	var updatedUser UserResponse
 	err = context.ShouldBindJSON(&updatedUser)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
+		util.ProvideResponse(context, 400, "could not parse request data")
 		return
 	}
 
 	updatedUser.Id = user.Id
 	err = updatedUser.Update()
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not update user"})
+		util.ProvideResponse(context, 500, "could not update user")
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{"message": "user updated successfully"})
+	util.ProvideResponse(context, 200, "user updated successfully")
 }
 
 func deleteUserHandler(context *gin.Context) {
 	userId, err := strconv.ParseInt(context.Param("id"), 10, 64)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse user id"})
+		util.ProvideResponse(context, 400, "could not parse user id")
 		return
 	}
 
 	user, err := getOne(userId)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch user"})
+		util.ProvideResponse(context, 500, "could not fetch user")
 		return
 	}
 
 	if user.Role == "admin" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "not authorized to delete user"})
+		util.ProvideResponse(context, 401, "not authorized to delete user")
 		return
 	}
 
 	err = user.Delete()
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "could not delete user"})
+		util.ProvideResponse(context, 400, "could not delete user")
 		return
 	}
-
-	context.JSON(http.StatusOK, gin.H{"message": "user deleted successfully"})
+	util.ProvideResponse(context, 200, "user deleted successfully")
 }
