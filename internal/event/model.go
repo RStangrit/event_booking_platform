39ef5b160fca6d6940f4c02a99346198de9cfdcd
flameprincess <unpredictableanonymous639@gmail.com>
2025-01-23 15:27:24 +0500

    added event struct, event routes

--- internal/event/model.go
+++ internal/event/model.go
@@ -1,1 +1,29 @@
 package event
+
+import "time"
+
+type EventRequest struct {
+	ID          int
+	Title       string `binding:"required"`
+	Description string `binding:"required"`
+	Date        string `binding:"required"`
+	Location    string `binding:"required"`
+	Capacity    int    `binding:"required"`
+	Price       float64
+	Created_by  int `binding:"required"`
+	Created_at  time.Time
+	Updated_at  *time.Time
+}
+
+type EventResponse struct {
+	ID          int
+	Title       string
+	Description string
+	Date        string
+	Location    string
+	Capacity    int
+	Price       float64
+	Created_by  int
+	Created_at  time.Time
+	Updated_at  *time.Time
+}
--- internal/event/routes.go
+++ internal/event/routes.go
@@ -1,1 +1,13 @@
 package event
+
+import "github.com/gin-gonic/gin"
+
+//for registering routes
+
+func RegisterRoutes(server *gin.Engine) {
+	server.POST("/events")
+	server.GET("/events")
+	server.GET("/events/:id")
+	server.PUT("/events/:id")
+	server.DELETE("/events/:id")
+}
